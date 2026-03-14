ed45554a312f45dfc3fcffa3f2ac6053bb67a319
PravinSK <pravinsk87@gmail.com>
2026-03-14 16:12:53 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name="AI-TRAVEL-PLANNER",
+    version="0.0.0",
+    author="Pravin SK",
+    author_email="pravinsk@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
+
+
