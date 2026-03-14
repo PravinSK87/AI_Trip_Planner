153f8e18959f36417ea0678809ba9b186c17ee8c
PravinSK <pravinsk87@gmail.com>
2026-03-14 16:30:12 +0530

    Code added or initial model loading

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,23 @@
+from utils.model_loader import ModelLoader
+from prompt_lib.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph,MessagesState,END,START
+from langgraph.prebuilt import ToolNode,tools_condition
+
+#from tools.weather_info import WeatherInfo
+#from tools.place_search import PlaceSearch
+#from tools.calculator import Calculator
+#from tools.currency_conv import CurrencyConverter
+
+
+class GraphBuilder():
+    def __init__(self):
+        pass
+
+    def agent_function(self):
+        pass
+
+    def build_graph(self):
+        pass
+
+    def __call__(self):
+        pass
